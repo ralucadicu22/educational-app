c97771bf4f6e4565e3062f8551ffee29eeeacbd9
Raluca <dicuraluca22@yahoo.com>
2024-11-28 14:56:33 +0200

    Initial commit- Spring Boot project

---
+++ educational-app/educational-app/src/main/java/com/example/educational_app/EducationalAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.educational_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationalAppApplication.class, args);
+	}
+
+}
---
+++ educational-app/educational-app/src/test/java/com/example/educational_app/EducationalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.educational_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
